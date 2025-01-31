3465fd432e23f0b76aee02c0e5c0ed1c3a555421
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-01-31 11:56:09 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class addition
+{
+    public:
+    int n1, n2;
+    addition ()
+    {
+        cin>>n1>>n2;
+        cout<<"Sum is "<<n1+n2;
+    }
+};
+int main()
+{
+    addition A;
+}
