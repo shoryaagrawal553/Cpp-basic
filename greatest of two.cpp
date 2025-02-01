3bd929b85e3133f0350937662815af42fea516da
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-01 20:50:54 +0530

    Add files via upload

---
+++ greatest of two.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter two numbers for the comparision ";
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<" "<<a<<" is the greatest number";
+    }
+    else
+    {
+        cout<<" "<<b<<" is the is greatest number";
+    }
+}
