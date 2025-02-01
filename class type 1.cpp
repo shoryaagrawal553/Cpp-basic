e2fa3171a50e8b480721fb0caca4aca5e5fa63d9
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-01 21:05:11 +0530

    Update class type 1.cpp

--- class type 1.cpp
+++ class type 1.cpp
@@ -1,45 +1,44 @@
 #include<iostream>
 using namespace std;
+
+// Define a class named 'student'
 class student
 {
     public:
+    // Member variables to store student information
     char name[100];
     int roll;
     float per;
+
     public:
+    // Member function to get student details from user input
     void get()
     {
         cout<<"\n Enter name ";
-        cin>>name;
+        cin>>name; // Get the student's name
         cout<<"\n Enter roll number ";
-        cin>>roll;
+        cin>>roll; // Get the student's roll number
         cout<<"\n Enter percentage ";
-        cin>>per;
+        cin>>per; // Get the student's percentage
     }
+
+    // Member function to display student details
     void show()
     {
-        cout<<"\n Name: "<<name;
-        cout<<"\n Rooll no.: "<<roll;
-        cout<<"\n Percentage: "<<per<<"%";
+        cout<<"\n Name: "<<name; // Display the student's name
+        cout<<"\n Rooll no.: "<<roll; // Display the student's roll number
+        cout<<"\n Percentage: "<<per<<"%"; // Display the student's percentage
     }
 };
+
 int main()
 {
+    // Create an object of the 'student' class
     student stu;
+
+    // Call member functions to get and display student details
     stu.get();
     stu.show();
+
+    return 0; // Return 0 to indicate successful execution
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
