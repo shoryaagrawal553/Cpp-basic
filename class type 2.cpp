b230a33d8de1391d476a9d45f614e63360ed8118
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-01 21:06:22 +0530

    Update class type 2.cpp

--- class type 2.cpp
+++ class type 2.cpp
@@ -1,43 +1,43 @@
 #include<iostream>
 using namespace std;
+
+// Define a class named 'student'
 class student
 {
-  char name[100];
-  int roll;
-  public:
-  void get();
-  void show();
+    // Private member variables to store student information
+    char name[100];
+    int roll;
+  
+    public:
+    // Member function declarations
+    void get();
+    void show();
 };
+
+// Member function definition to get student details from user input
 void student :: get()
 {
     cout<<"Enter name ";
-    cin>>name;
+    cin>>name; // Get the student's name
     cout<<" Enter roll no. ";
-    cin>>roll;
+    cin>>roll; // Get the student's roll number
 }
+
+// Member function definition to display student details
 void student :: show()
 {
-    cout<<"\n Name: "<<name;
-    cout<<"\n Roll no.: "<<roll;
+    cout<<"\n Name: "<<name; // Display the student's name
+    cout<<"\n Roll no.: "<<roll; // Display the student's roll number
 }
+
 int main()
 {
+    // Create an object of the 'student' class
     student s;
+
+    // Call member functions to get and display student details
     s.get();
     s.show();
+
+    return 0; // Return 0 to indicate successful execution
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
