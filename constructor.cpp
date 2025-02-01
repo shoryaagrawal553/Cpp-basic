bdb7677d42dfbf3429d7a7c0b147b8d79761b9d0
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-01 21:07:18 +0530

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,16 +1,25 @@
 #include<iostream>
 using namespace std;
+
+// Define a class named 'addition'
 class addition
 {
     public:
+    // Member variables to store two integers
     int n1, n2;
+    
+    // Constructor to initialize member variables and calculate the sum
     addition ()
     {
-        cin>>n1>>n2;
-        cout<<"Sum is "<<n1+n2;
+        cin >> n1 >> n2; // Get two integers from user input
+        cout << "Sum is " << n1 + n2; // Display the sum of the two integers
     }
 };
+
 int main()
 {
-    addition A;
+    // Create an object of the 'addition' class
+    addition A; // The constructor is called automatically here
+
+    return 0; // Return 0 to indicate successful execution
 }
