46112fb251a8914ea11ececf40f715220992113f
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-02 12:36:00 +0530

    Update hierarchial inheritace.cpp

--- hierarchial inheritace.cpp
+++ hierarchial inheritace.cpp
@@ -1,54 +1,58 @@
 #include<iostream>
 using namespace std;
+
+// Class A definition
 class A 
 {
-    public:
+public:
     int n1, n2;
+    
+    // Method to get two numbers from user
     void get()
     {
-        cout<<"\nEnter two numbers ";
-        cin>>n1>>n2;
+        cout << "\nEnter two numbers ";
+        cin >> n1 >> n2;
     }
 };
+
+// Class B inherits from class A
 class B: public A
 {
-    public:
+public:
     int sum;
+    
+    // Method to calculate and display the sum of two numbers
     void Sum()
     {
-        sum=n1+n2;
-        cout<<"Sum is "<<sum;
+        sum = n1 + n2;
+        cout << "Sum is " << sum;
     }
 };
+
+// Class C inherits from class A
 class C: public A
 {
-    public:
+public:
     int sub;
+    
+    // Method to calculate and display the difference of two numbers
     void Sub()
     {
-        sub=n1-n2;
-        cout<<"\nDifference is "<<sub;
+        sub = n1 - n2;
+        cout << "\nDifference is " << sub;
     }
 };
+
 int main()
 {
-   B b;
-   C c;
-   b.get();
-   b.Sum();
-   c.get();
-   c.Sub();
+    B b;
+    C c;
+    
+    // Get numbers and display sum using object of class B
+    b.get();
+    b.Sum();
+    
+    // Get numbers and display difference using object of class C
+    c.get();
+    c.Sub();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
