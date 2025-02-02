a8d36161f45b566eac1b3c31ef1873d616a307d0
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-02 12:31:01 +0530

    Update function overloading 2.cpp

--- function overloading 2.cpp
+++ function overloading 2.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+
+// Class A definition
 class A
 {
 public:
     int n, i, j;
+    
+    // Method to get a number from user and print a pattern of asterisks
     void get()
     {
         cout << "Enter a number ";
@@ -18,10 +22,14 @@ public:
         }
     }
 };
+
+// Class B inherits from class A
 class B : public A
 {
 public:
     int n, i, j;
+    
+    // Method to get a number from user and print a pattern of numbers
     void get()
     {
         cout << "\nEnter a number ";
@@ -36,10 +44,20 @@ public:
         }
     }
 };
+
 int main()
 {
-    B b;        // if class B hav not inherited class A then it will be
-    b.A::get(); // A a;
-    b.B::get(); // B b;
-} // a.A::get();
-  // b.B::get();
+    B b;
+    
+    // Call the get method from class A using the object of class B
+    b.A::get();
+    
+    // Call the get method from class B using the object of class B
+    b.B::get();
+    
+    // Uncomment and use the following if class B had not inherited from class A
+    // A a;
+    // B b;
+    // a.A::get();
+    // b.B::get();
+}
