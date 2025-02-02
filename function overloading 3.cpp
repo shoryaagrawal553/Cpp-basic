59c1e2488fa5e67013e58cb4d437f9a78618b383
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-02 12:32:15 +0530

    Update function overloading 3.cpp

--- function overloading 3.cpp
+++ function overloading 3.cpp
@@ -1,55 +1,48 @@
 #include<iostream>
 using namespace std;
+
+// Class demo definition
 class demo
 {
-    public:
+public:
     int sum, n1, n2;
+    
+    // Parameterized constructor to initialize n1 and n2 with given values
     demo(int a, int b)
     {
-        n1=a;
-        n2=b;
+        n1 = a;
+        n2 = b;
     }
+    
+    // Default constructor to read two numbers from user and calculate their sum
     demo()
     {
-        cout<<"Enter two numbers";
-        cin>>n1>>n2;
-        sum=n1+n2;
+        cout << "Enter two numbers";
+        cin >> n1 >> n2;
+        sum = n1 + n2;
     }
+    
+    // Copy constructor to initialize n1 and n2 with values from another demo object
     demo(demo& s)
-{
-    n1=s.n1;
-    n2=s.n2;
-   // cout<<"\nhiiiiiiiii";
-}
-void show()
-{
-    cout<<"\nSum is "<<sum;
-}
+    {
+        n1 = s.n1;
+        n2 = s.n2;
+    }
+    
+    // Method to display the sum
+    void show()
+    {
+        cout << "\nSum is " << sum;
+    }
 };
+
 int main()
 {
+    // Create an object of demo using default constructor
     demo d;
     d.show();
+    
+    // Create another object of demo using copy constructor
     demo y(d);
     d.show();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
