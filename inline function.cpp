9a369dcae835d5366c56eb850709d45f09dd3cd2
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-04 21:40:08 +0530

    Update inline function.cpp

--- inline function.cpp
+++ inline function.cpp
@@ -1,13 +1,72 @@
 #include<iostream>
 using namespace std;
-inline int add(int a, int b)    //program will work even without inline
+
+// Base class A
+class A 
 {
-    return (a+b);
-}
+    public:
+    int n1, n2;
+
+    // Function to get two numbers from user
+    void get()
+    {
+        cout<<"Enter two numbers ";
+        cin>>n1>>n2;
+    }
+};
+
+// Derived class B inherits from A virtually
+class B: virtual public A
+{
+    public:
+    int sum;
+
+    // Function to add the two numbers
+    void add()
+    {
+        sum = n1 + n2;
+    }
+};
+
+// Derived class C inherits from A virtually
+class C: virtual public A
+{
+    public:
+    int diff;
+
+    // Function to find the difference of the two numbers
+    void sub()
+    {
+        diff = n1 - n2;
+    }
+};
+
+// Derived class D inherits from B and C
+class D: public B, public C
+{
+    public:
+
+    // Function to display the sum and difference
+    void show()
+    {
+        cout << "Sum is " << sum;
+        cout << "\nDifference is " << diff;
+    }
+};
+
 int main()
 {
-    int a, b;
-    cout<<"Enter two numbers ";
-    cin>>a>>b;
-    cout<<"Sum of the numbers is "<<add(a, b);
+    D d;
+
+    // Get numbers from user
+    d.get();
+
+    // Calculate sum
+    d.add();
+
+    // Calculate difference
+    d.sub();
+
+    // Display results
+    d.show();
 }
