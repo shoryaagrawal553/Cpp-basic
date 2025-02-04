5707dda9730b3c80d8b2cf007839c50d4169c917
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-04 21:41:10 +0530

    Update multilevel inheritance.cpp

--- multilevel inheritance.cpp
+++ multilevel inheritance.cpp
@@ -1,55 +1,61 @@
 #include<iostream>
 using namespace std;
+
+// Base class b
 class b 
 {
-public:
-int n1;
-public:
-void get()
-{
-    cout<<"\nEnter a number ";
-    cin>>n1;
-}
+    public:
+    int n1;
+
+    // Function to get a number from user
+    public:
+    void get()
+    {
+        cout << "\nEnter a number ";
+        cin >> n1;
+    }
 };
+
+// Intermediate class i inherits from class b
 class i : public b
 {
     public:
     int n2;
+
+    // Function to get another number from user
     public:
     void get2()
     {
-       cout<<"\nEnter another number ";
-       cin>>n2;
+        cout << "\nEnter another number ";
+        cin >> n2;
     }
 };
+
+// Derived class inherits from class i
 class derived : public i
 {
     public:
     int sum;
+
+    // Function to calculate and display the sum of the two numbers
     public:
     void show()
     {
-        sum=n1+n2;
-        cout<<"\nSum is "<<sum;
+        sum = n1 + n2;
+        cout << "\nSum is " << sum;
     }
 };
+
 int main()
 {
     derived d;
+
+    // Get first number from user
     d.get();
+
+    // Get second number from user
     d.get2();
+
+    // Display the sum of the two numbers
     d.show();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
