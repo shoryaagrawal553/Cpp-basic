30a49f6c8d4d2338179e64bb2ac0fa0f0681f6e3
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-04 21:50:55 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
     int a, b;
-    cout<<"Enter two numbers ";
-    cin>>a>>b;
-    cout<<"Sum is "<<a+b;
+
+    // Prompt the user to enter two numbers
+    cout << "Enter two numbers ";
+    cin >> a >> b;
+
+    // Calculate and display the sum of the two numbers
+    cout << "Sum is " << a + b;
 }
