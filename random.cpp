92d3bbe6de79e703762956067dacd478c1ddd5bd
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-04 21:48:31 +0530

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -1,60 +1,56 @@
 #include<iostream>
 using namespace std;
+
+// Base class employee
 class employee
 {
     public:
     int id;
     char name[100];
+
+    // Constructor to get employee's name and ID from user
     employee ()
     {
-        cout<<"\nEnter your name and Id ";
-        cin>>name>>id;
+        cout << "\nEnter your name and Id ";
+        cin >> name >> id;
     }
 };
+
+// Derived class manager inherits from employee privately
 class manager: private employee
 {
     public:
     int mob;
+
+    // Constructor to get manager's mobile number and display details
     manager()
     {
-      //  employee();
-        cout<<"\nEnter our mobile number ";
-        cin>>mob;
-        cout<<"\nName: "<<name<<"\nId: "<<id;
-        cout<<"\nMobile no.: "<<mob;
+        // employee(); // This line is commented out as the base class constructor is called automatically
+        cout << "\nEnter your mobile number ";
+        cin >> mob;
+        cout << "\nName: " << name << "\nId: " << id;
+        cout << "\nMobile no.: " << mob;
     }
 };
+
+// Derived class accountant inherits from employee publicly
 class accountant: public employee
 {
     public:
     int sal;
+
+    // Constructor to get accountant's salary and display details
     accountant()
     {
-     cout<<"\nEnter your salary ";
-     cin>>sal;
-     cout<<"\nName: "<<name<<"\nId: "<<id;
-     cout<<"\nSalary: "<<sal;
+        cout << "\nEnter your salary ";
+        cin >> sal;
+        cout << "\nName: " << name << "\nId: " << id;
+        cout << "\nSalary: " << sal;
     }
 };
+
 int main()
 {
-    manager m;
-    accountant a;
+    manager m;  // Create manager object
+    accountant a;  // Create accountant object
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
